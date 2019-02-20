e6e4cce1141a9673c1f92495bc6dfced7fc150ab
kacperpochec <thebromn@yahoo.com>
2019-02-20 10:53:25 +0100

    dodanie query

--- src/main/java/pl/alfasoft/immo/property/PropertyRepository.java
+++ src/main/java/pl/alfasoft/immo/property/PropertyRepository.java
@@ -3,10 +3,11 @@ package pl.alfasoft.immo.property;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PropertyRepository extends JpaRepository<Property, Long> {
-
+    @Query(value = "SELECT * FROM PROPERTIES WHERE FL_ID = ?1", nativeQuery = true)
     Page<Property> findPropertiesByAddress_Id(Pageable pageable,  Long addressId);
 }
