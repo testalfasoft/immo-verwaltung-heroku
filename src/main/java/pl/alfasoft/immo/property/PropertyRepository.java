da77f9b0a9747ada442b7a7bbee5837202d41ba6
kacperpochec <thebromn@yahoo.com>
2019-02-20 12:54:05 +0100

    dodanie query3

--- src/main/java/pl/alfasoft/immo/property/PropertyRepository.java
+++ src/main/java/pl/alfasoft/immo/property/PropertyRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PropertyRepository extends JpaRepository<Property, Long> {
-    @Query(value = "SELECT fl_id FROM Property ORDER BY fl_id DESC")
+    @Query(value = "SELECT id FROM Property ORDER BY id DESC")
     Page<Property> findAllByPropertiesByAddress_Id(Pageable pageable, Long addressId);
 }
