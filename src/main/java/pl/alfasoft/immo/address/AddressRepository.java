72296ed6a31e1753b13bbc1a666abe69211a54a8
kacperpochec <thebromn@yahoo.com>
2019-02-20 13:19:29 +0100

    dodanie query3

--- src/main/java/pl/alfasoft/immo/address/AddressRepository.java
+++ src/main/java/pl/alfasoft/immo/address/AddressRepository.java
@@ -1,9 +1,14 @@
 package pl.alfasoft.immo.address;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import pl.alfasoft.immo.property.Property;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
-
+    @Query(value = "SELECT id FROM Address ORDER BY id DESC")
+    Page<Address> findAll(Pageable pageable);
 }
