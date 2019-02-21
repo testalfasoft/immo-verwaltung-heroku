8bdd96ec9375d62b67b08d6940293b8abdce3f34
kacperpochec <thebromn@yahoo.com>
2019-02-21 12:38:28 +0100

    lista adresow od najnowszego

--- src/main/java/pl/alfasoft/immo/address/AddressRepository.java
+++ src/main/java/pl/alfasoft/immo/address/AddressRepository.java
@@ -9,6 +9,6 @@ import pl.alfasoft.immo.property.Property;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
-    @Query(value = "SELECT id FROM Address ORDER BY id ASC")
+    @Query(value = "SELECT id FROM Address ORDER BY id DESC")
     Page<Address> getAddress(Pageable pageable, Long id);
 }
