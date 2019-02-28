c01c961d4c250a1c70290e73cabb1ddd4c706785
kacperpochec <thebromn@yahoo.com>
2019-02-28 11:37:16 +0100

    xd

--- src/main/java/pl/alfasoft/immo/address/AddressRepository.java
+++ src/main/java/pl/alfasoft/immo/address/AddressRepository.java
@@ -9,6 +9,6 @@ import pl.alfasoft.immo.property.Property;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
-    @Query(value = "SELECT id FROM Address ORDER BY id DESC")
+    @Query(value = "SELECT id FROM Address ORDER BY id ASC")
     Page<Address> findAllByAddressByAddress_Id(Pageable pageable, Long id);
 }
