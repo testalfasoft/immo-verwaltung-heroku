fb0f5705d2619cfffbf7bc850f30db7f5c3e8b6b
kacperpochec <thebromn@yahoo.com>
2019-02-28 11:32:23 +0100

    xd

--- src/main/java/pl/alfasoft/immo/address/AddressController.java
+++ src/main/java/pl/alfasoft/immo/address/AddressController.java
@@ -105,7 +105,7 @@ public class AddressController {
                 .headers(HeaderUtil.createEntityDeletionAlert(ENTITY_NAME, id.toString()))
                 .build();
     }
-    @GetMapping("/address/add/{id}")
+    @RequestMapping("/address/add/{id}")
     public ResponseEntity<List<Address>> findAllByAddressId(Pageable pageable, @PathVariable Long id) {
 
         log.debug("REST request to get Property By Address id : {}", id);
